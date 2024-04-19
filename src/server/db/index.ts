4a45034ab48317b6de11e225b21cfd30a9b38590
Bishal Neupane <Neupanebishal07@gmail.com>
2024-04-19 09:56:55 +0545

    neon added

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,11 +1,19 @@
-import postgres from "postgres";
+// import postgres from "postgres";
+// import { env } from "@/env";
 import { env } from "@/env";
 import * as schema from "./schema";
-import { drizzle } from "drizzle-orm/postgres-js";
+// import { drizzle } from "drizzle-orm/postgres-js";
 
-export const connection = postgres(env.POSTGRES_URL, {
-  max_lifetime: 10, // Remove this line if you're deploying to Docker / VPS
-  // idle_timeout: 20, // Uncomment this line if you're deploying to Docker / VPS
-});
+// export const connection = postgres(env.DATABASE_URL, {
+//   max_lifetime: 10, // Remove this line if you're deploying to Docker / VPS
+//   // idle_timeout: 20, // Uncomment this line if you're deploying to Docker / VPS
+// });
 
-export const db = drizzle(connection, { schema });
+// export const db = drizzle(connection, { schema });
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(env.DATABASE_URL);
+
+export const db = drizzle(sql, { schema });
