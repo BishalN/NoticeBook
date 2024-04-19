50f3397df99ca1bba2d636ceab88f57a64a6d42a
Bishal Neupane <Neupanebishal07@gmail.com>
2024-04-19 19:33:27 +0545

    update header with proper logo

--- src/app/(landing)/_components/header.tsx
+++ src/app/(landing)/_components/header.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { RocketIcon } from "@/components/icons";
-import { APP_TITLE } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -9,6 +7,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 
 const routes = [
   { name: "Home", href: "/" },
@@ -39,10 +38,12 @@ export const Header = () => {
             </div>
           </DropdownMenuContent>
         </DropdownMenu>
-        <Link className="flex items-center justify-center text-xl font-medium" href="/">
-          <RocketIcon className="mr-2 h-5 w-5" /> {APP_TITLE}
+        <Link className="flex items-center justify-center px-3 text-xl font-medium" href="/">
+          {/* <RocketIcon className="mr-2 h-5 w-5" /> {APP_TITLE} */}
+          <Image alt="Logo" src="/logo.png" height={200} width={200} className="h-7 w-7 " />{" "}
+          {/* {APP_TITLE} */}
         </Link>
-        <nav className="ml-10 hidden gap-4 sm:gap-6 md:flex">
+        <nav className="ml-5 hidden gap-4 sm:gap-6 md:flex">
           {routes.map(({ name, href }) => (
             <Link
               key={name}
